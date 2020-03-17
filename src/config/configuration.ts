2fa150988e27aceb07d9d2a54b4fea4e25704a62
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-03-17 19:22:40 +0200

    add amqp consumer

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
 	DATA_SERVICE_URL: process.env.DATA_SERVICE_URL || 'http://localhost:3001',
 	SCRAPER_SERVICE_URL: process.env.SCRAPER_SERVICE_URL || 'http://localhost:8085',
-	WORKER_QUEUE_NAME: process.env.WORKER_QUEUE_NAME || 'PIPELINE_TASK_RUN1',
+	WORKER_QUEUE_NAME: process.env.WORKER_QUEUE_NAME || 'DEV_PIPELINE_TASK_RUN_NODE',
 	AMQP_CONNECTION_STRING: process.env.AMQP_CONNECTION_STRING || 'amqp://user:password@k8s:30403',
 	AMQP_LOG_LEVEL: process.env.AMQP_LOG_LEVEL || 'info',
 });
