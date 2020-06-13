1fc934497c4539a9bbaee26cc1ec2e68163964f9
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-06-13 18:35:07 +0300

    hover doc

--- src/core/pipeline/v2.0/command/hover.command.ts
+++ src/core/pipeline/v2.0/command/hover.command.ts
@@ -1,6 +1,8 @@
 import { AbstractCommand } from '../../command/abstract-command';
 import { Assignable } from '../../command/decorator/assignable.decorator';
+import { Cmd } from '../../command/decorator/command.decorator';
 
+@Cmd({cmd: 'hover', version: '2.0'})
 export class HoverCommand extends AbstractCommand {
 
 	@Assignable({required: false})
