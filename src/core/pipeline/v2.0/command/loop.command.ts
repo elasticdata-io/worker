8d77dd036cc98612bf6a9c5b20917d98848074cd
sergey tkachenko <serg.tkachenko@hotmail.com>
2020-08-03 22:42:20 +0300

    loop add currentIndex to analyzed

--- src/core/pipeline/v2.0/command/loop.command.ts
+++ src/core/pipeline/v2.0/command/loop.command.ts
@@ -72,7 +72,7 @@ export class LoopCommand extends AbstractCommand {
 
 	public getManagedKeys(): Array<{key: string, fn: () => Promise<string> } | string> {
 		const keys = super.getManagedKeys();
-		return keys.concat(['context', 'currentIndex']);
+		return keys.concat(['max', 'context', 'index', 'currentIndex']);
 	}
 
 	/**
