c244711fc0ee6cf1b488c699070b51d790d9f32c
sergey tkachenko <serg.tkachenko@hotmail.com>
2020-10-06 21:39:01 +0300

    added pipelineSettings

--- src/dto/run.task.dto.ts
+++ src/dto/run.task.dto.ts
@@ -1,7 +1,25 @@
+interface PipelineUserInteractionSettings {
+	watchSelectors: string[];
+}
+
+interface PipelineWindowSettings {
+	width: number;
+	height: number;
+	lang: string;
+}
+
+interface PipelineSettings {
+	maxWorkingMinutes: number;
+	window: PipelineWindowSettings;
+	proxies: string[];
+	userInteraction: PipelineUserInteractionSettings
+}
+
 export class RunTaskDto {
 	json: string;
 	taskId: string;
 	pipelineId: string;
+	pipelineSettings: PipelineSettings;
 	userUuid: string;
 	proxies: string[];
 }
