489f7efa736c049f67d11b67a84dc3aa01180e30
s.tkachenko <serg.tkachenko@hotmail.com>
2021-01-24 23:17:41 +0200

    FROM_CONTAINER_KEY

--- src/alive/alive.service.ts
+++ src/alive/alive.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { Interval, Timeout } from '@nestjs/schedule';
+import { Interval } from '@nestjs/schedule';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -14,12 +14,12 @@ export class AliveService {
 	public async handleCron(): Promise<void> {
 		const WORKER_TYPE = this.configService.get<string>('WORKER_TYPE');
 		const WORKER_MANAGER_URL = this.configService.get<string>('WORKER_MANAGER_URL');
-		const CONTAINER_KEY = this.configService.get<string>('CONTAINER_KEY');
+		const FROM_CONTAINER_KEY = this.configService.get<string>('FROM_CONTAINER_KEY');
 		const url = `${WORKER_MANAGER_URL}/worker/alive`;
 		try {
 			await axios.post(url, {
 				userId: WORKER_TYPE,
-				containerKey: CONTAINER_KEY,
+				containerKey: FROM_CONTAINER_KEY,
 			});
 		} catch (e) {
 			this.logger.error(e);
