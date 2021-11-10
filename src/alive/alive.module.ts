351d208dcfe2ea422c1bc4b79ca8da30756f3443
s.tkachenko <serg.tkachenko@hotmail.com>
2021-11-10 02:07:23 +0200

    remove alive service

--- src/alive/alive.module.ts
+++ src/alive/alive.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
-import { AliveService } from './alive.service';
 import { EnvModule } from '../env/env.module';
 
 @Module({
@@ -10,6 +9,6 @@ import { EnvModule } from '../env/env.module';
 		ConfigModule.forRoot(),
 		EnvModule,
 	],
-	providers: [AliveService,]
+	providers: []
 })
 export class AliveModule {}
